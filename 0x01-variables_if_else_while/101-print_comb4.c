2231496f1bad190c48f784709f28b26385db8f1d
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-01 20:27:40 -0700

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int L, R, C;
+
+	for (L = 48; L <= 57; L++)
+	{
+		for (C = L + 1; C <= 57; C++)
+		{
+			for (R = C + 1; R <= 57; R++)
+
+			{
+				putchar(L);
+					putchar(C);
+					putchar(R);
+
+					if ((L == 55) && (C == L + 1) && (R == C + 1))
+					{
+						break;}
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
