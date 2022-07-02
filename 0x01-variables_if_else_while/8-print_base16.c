e5369b35bfcc9224ffb6634d813b3939bbddb8b9
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-01 19:52:42 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	
+	putchar('\n');
+	return (0);
+}
