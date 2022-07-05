e1846411a194982bd1fafbb4bbcd3bbde16884d8
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-05 09:11:32 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+		return (0);
+	}
+}
+
