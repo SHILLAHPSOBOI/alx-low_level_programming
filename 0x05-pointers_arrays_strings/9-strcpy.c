32489a44aa89507004a471eafcadbcb8b18ac9e7
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-11 11:57:12 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies string by src
+ * @dest: pointer of a char to be updated
+ * @scr: pointer ofa char to be updated
+ *
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+
+{
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i+;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+
+}
