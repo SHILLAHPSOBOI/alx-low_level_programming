56fb0dca89aec596d15fdce8cbd664c3a1948459
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-11 11:53:25 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: pointer updated
+ * @n: return value
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+
+		i++;
+	}
+
+	printf('\n');
+}
