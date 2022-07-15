75e017b2fe0db99be8b464e3ff51f886f174f010
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-14 19:34:51 -0700

    corrected

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,14 +18,14 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	while (*(n2 + c2) != '\0')
 		c2++;
 	if (c1 >= c2)
-		bg =c1;
+		bg = c1;
 	else
 		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
 	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
-	dr1 = *(n1 +c1) - 48, dr2 = *(n2 + c2) - 48;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
 	{
 		op = dr1 + dr2 + add;
@@ -41,7 +41,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			c1--, dr1 = *(n1 + c1) - 48;
 		else
 			dr1 = 0;
-		if (c2 >0)
+		if (c2 > 0)
 			c2--, dr2 = *(n2 + c2) - 48;
 		else
 			dr2 = 0;
