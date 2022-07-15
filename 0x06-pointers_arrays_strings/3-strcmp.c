80283d2bd0596b1dbd1c7f07aa176509fe6e0dcf
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-14 18:41:35 -0700

    corrected

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 	while (*str_one != '\0' && *str_two != '\0' && *str_one == *str_two)
 	{
 		str_one++;
-		str_two++
+		str_two++;
 	}
 
 	return (*str_one - *str_two);
