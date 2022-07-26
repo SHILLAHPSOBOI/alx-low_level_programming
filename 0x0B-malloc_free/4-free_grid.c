957d7f9fc14f90d089bbe316e781dc261041b714
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-26 02:42:50 -0700

    file 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * free_grid - allocates grid
+ * @grid: takes width
+ * @height: height of grid
+ * Return: free grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
