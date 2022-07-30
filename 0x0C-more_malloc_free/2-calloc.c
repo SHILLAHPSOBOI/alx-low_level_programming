a75e317c56c5d67ebba8d3f23a95dec1d4c9aeb8
SHILLAHPSOBOI <shillahpsoboi@gmail.com>
2022-07-30 00:07:14 -0700

    file 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,10 +13,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *mem;
 	char *filler;
 	unsigned int index;
-	
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-
 	mem = malloc(size * nmemb);
 	if (mem == NULL)
 		return (NULL);
@@ -27,6 +26,5 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		filler[index] = '\0';
 	}
-	
 	return (mem);
 }
